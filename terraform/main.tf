666ff9d8968d0e7f4e1fc2f4a057a2149bd2b97d
Pedro Haramoto <pedroharamoto@gmail.com>
2024-01-22 23:30:36 -0300

    fix ingress rule SG

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,10 +87,10 @@ resource "aws_lambda_event_source_mapping" "pedidos_sqs_trigger" {
 
 #Add ingress rule to ALB
 resource "aws_security_group_rule" "ingress_rule_alb" {
-  security_group_id = var.security_group_alb
-  type              = "ingress"
-  from_port         = 8000
-  to_port           = 8000
-  protocol          = "tcp"
-  cidr_blocks       = [aws_security_group.security_group_lambda_pedidos.id]
+  security_group_id        = var.security_group_alb
+  type                     = "ingress"
+  from_port                = 8000
+  to_port                  = 8000
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.security_group_lambda_pedidos.id
 }
